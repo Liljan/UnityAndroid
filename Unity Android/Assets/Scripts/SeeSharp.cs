97564a4338f37deab8a67556bf0dfa1f84d59ad6
Liljan <marcus.t.lilja@gmail.com>
2016-10-25 21:22:10 +0200

    Initial commit.

---
+++ Unity Android/Assets/Scripts/SeeSharp.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeeSharp : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Can you see sharp?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
